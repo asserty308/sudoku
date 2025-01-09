38d90443a925f625680bc1281e76fe10c93d7066
asserty308 <yannickassert@gmail.com>
2025-01-09 07:07:29 +0100

    refactor: simplify theme configuration and improve home page layout

--- lib/app/ui/styles/theme.dart
+++ lib/app/ui/styles/theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData get lightTheme => ThemeData(
   useMaterial3: true,
-  fontFamily: _fontFamily,
+  // fontFamily: _fontFamily,
   colorScheme: _colorScheme(false),
   textTheme: _textTheme,
   textButtonTheme: _textButtonTheme(true),
@@ -10,13 +10,13 @@ ThemeData get lightTheme => ThemeData(
 
 ThemeData get darkTheme => ThemeData(
   useMaterial3: true,
-  fontFamily: _fontFamily,
+  // fontFamily: _fontFamily,
   colorScheme: _colorScheme(true),
   textTheme: _textTheme,
   textButtonTheme: _textButtonTheme(true),
 );
 
-const String _fontFamily = 'IndieFlower';
+// const String _fontFamily = 'IndieFlower';
 
 ColorScheme _colorScheme(bool isDarkMode) => ColorScheme.fromSeed(
   seedColor: Colors.deepPurple, 
@@ -31,6 +31,6 @@ TextButtonThemeData? _textButtonTheme(bool isDarkMode) => TextButtonThemeData(
   style: TextButton.styleFrom(
     foregroundColor: isDarkMode ? Colors.white : null, // use default color on light mode
     disabledForegroundColor: isDarkMode ? Colors.white54 : null, // use default color on light mode
-    textStyle: TextStyle(fontFamily: _fontFamily),
+    // textStyle: TextStyle(fontFamily: _fontFamily),
   ),
 );
--- lib/home/ui/pages/home_page.dart
+++ lib/home/ui/pages/home_page.dart
@@ -14,28 +14,25 @@ class _HomePageState extends State<HomePage> {
     body: _body,
   );
 
-  Widget get _body => ListView(
-    physics: ClampingScrollPhysics(),
+  Widget get _body => Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    spacing: 24,
     children: [
-      _title,
+      Text('Sudoku', style: Theme.of(context).textTheme.headlineMedium,),
+      Text('Challenge your mind with every grid.'),
       _playButton,
     ],
   );
 
-  Widget get _title => Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Text('Sudoku', style: Theme.of(context).textTheme.headlineMedium,),
-      Text('Challenge your mind with every grid.'),
-    ],
-  );
-
-  Widget get _playButton => TextButton(
-    onPressed: () => context.go('/play'), 
-    style: TextButton.styleFrom(
-      shape: StadiumBorder(),
-      backgroundColor: Colors.blueGrey[400],
+  Widget get _playButton => Center(
+    child: TextButton(
+      onPressed: () => context.go('/play'), 
+      style: TextButton.styleFrom(
+        shape: StadiumBorder(),
+        backgroundColor: Colors.blueGrey[400],
+        minimumSize: Size(200, 50),
+      ),
+      child: Text('Play'),
     ),
-    child: Text('Play'),
   );
 }
