12a24fe431bfcb2b25f3a6fb53d566024596f0e9
Yannick Assert <asserty308@icloud.com>
2026-08-28 08:54:15 +0200

    refactor: consolidate app structure by removing SudokuMaterialApp and integrating its functionality into MyApp

--- lib/core/ui/app.dart
+++ lib/core/ui/app.dart
@@ -2,7 +2,9 @@ import 'package:flutter_core/flutter_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:sudoku/core/navigation/router.dart';
-import 'package:sudoku/core/ui/material_app.dart';
+import 'package:sudoku/core/theme/theme.dart';
+import 'package:sudoku/l10n/gen/app_localizations.dart';
+import 'package:sudoku/l10n/l10n.dart';
 
 class const MyApp({super.key}) extends ConsumerStatefulWidget {
   @override
@@ -11,6 +13,17 @@ class const MyApp({super.key}) extends ConsumerStatefulWidget {
 
 class _MyAppState() extends AppConsumerState<MyApp> {
   @override
-  Widget build(BuildContext context) =>
-      SudokuMaterialApp(routerConfig: ref.read(goRouterProvider));
+  Widget build(BuildContext context) => MaterialApp.router(
+    restorationScopeId: 'app',
+    onGenerateTitle: (context) => context.l10n.appTitle,
+    debugShowCheckedModeBanner: false,
+    theme: lightTheme,
+    darkTheme: darkTheme,
+    routerConfig: ref.read(goRouterProvider),
+    localizationsDelegates: [
+        AppLocalizations.delegate, // Add this line
+        ...GlobalMaterialLocalizations.delegates,
+      ],
+    supportedLocales: AppLocalizations.supportedLocales,
+  );
 }
